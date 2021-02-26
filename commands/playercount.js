4f05d9a5d9312783ebc4a3e5e200cf81a4f63220
Nick <65097490+Nick-NCSU@users.noreply.github.com>
2021-02-26 16:13:07 -0500

    Updated playercount
    Added sorting to playerCount. Filters out games with 0 players.

--- commands/playercount.js
+++ commands/playercount.js
@@ -13,35 +13,51 @@ module.exports.run = async (bot, message, args) => {
   .then(res => res.json())
   .then(data => apiJSON = data)
   .then(() => {
+    let gamesList = [
+      ["MAIN_LOBBY", "Main Lobby"],
+      ["TOURNAMENT_LOBBY", "Tournament Lobby"],
+      ["LEGACY", "Classic"],
+      "SPEED_UHC", "Speed UHC"],
+      ["REPLAY", "Replays"],
+      ["PROTOTYPE", "Prototype"],
+      ["SUPER_SMASH", "Smash Heroes"],
+      ["DUELS", "Duels"],
+      ["WALLS3", "Mega Walls"],
+      ["BEDWARS", "BedWars"],
+      ["PIT", "The Pit"],
+      ["UHC", "UHC"],
+      ["SKYWARS", "SkyWars"],
+      ["SKYBLOCK", "SkyBlock"],
+      ["BUILD_BATTLE", "Build Battle"],
+      ["MCGO", "CvC"],
+      ["BATTLEGROUND", "Warlords"],
+      ["MURDER_MYSTERY", "Murder Mystery"],
+      ["TNTGAMES", "TNT Games"],
+      ["ARCADE", "Arcade"],
+      ["HOUSING", "Housing"],
+      ["SURVIVAL_GAMES", "Blitz SG"],
+      ["LIMBO", "Limbo"],
+      ["IDLE", "Idle"],
+      ["QUEUE", "Queue"]
+    ];
+    let gameCount = [];
+    for(game of gamesList){
+      gameCount.push([game[1], apiJSON[game[0]].players]);
+    }
+    gameCount.sort(function(a, b) {
+        return b[1] - a[1];
+    });
+    gameCount = gameCount.filter(entry => entry[1] != 0);
     let playerCount = JSON.stringify(apiJSON.playerCount)
-    let MainLobby = JSON.stringify(apiJSON.games.MAIN_LOBBY.players)
-    let Skyblock = JSON.stringify(apiJSON.games.SKYBLOCK.players)
-    let Bedwars = JSON.stringify(apiJSON.games.BEDWARS.players)
-    let Duels = JSON.stringify(apiJSON.games.DUELS.players)
-    let buildBattle = JSON.stringify(apiJSON.games.BUILD_BATTLE.players)
-    let Pit = JSON.stringify(apiJSON.games.PIT.players)
-    let Housing = JSON.stringify(apiJSON.games.HOUSING.players)
-    let murderMystery = JSON.stringify(apiJSON.games.MURDER_MYSTERY.players)
-    let TNTGAMES = JSON.stringify(apiJSON.games.TNTGAMES.players)
-    let Blitz = JSON.stringify(apiJSON.games.SURVIVAL_GAMES.players)
-    let Warlords = JSON.stringify(apiJSON.games.BATTLEGROUND.players)
-    let classicGames = JSON.stringify(apiJSON.games.LEGACY.players)
-    let speedUHC  = JSON.stringify(apiJSON.games.SPEED_UHC.players)
-    let UHC  = JSON.stringify(apiJSON.games.UHC.players)
-    let prototype  = JSON.stringify(apiJSON.games.PROTOTYPE.players)
-    let smashHeros  = JSON.stringify(apiJSON.games.SUPER_SMASH.players)
-    let megaWalls  = JSON.stringify(apiJSON.games.WALLS3.players)
-    let MCGO  = JSON.stringify(apiJSON.games.MCGO.players)
     
 
     const embed = new Discord.MessageEmbed()
    .setTitle(`**Player Count » ${playerCount}**`)
    .setThumbnail(`https://cdn.discordapp.com/attachments/792226377113796618/814930561671757905/AAUvwnjTWypaip_yzS5ajG97GEJ5YGSZnvan4CxFv0KZ6gs900-c-k-c0x00ffffff-no-rj.png`)
    .setColor(randomColor)
-    .addFields(
-    {name: `\`•\` Skyblock: **${Skyblock}** \n\`•\` Main Lobby: **${MainLobby}** \n\`•\` Bedwars: **${Bedwars}** \n\`•\` Duels: **${Duels}** \n\`•\` Build Battle: **${buildBattle}** \n\`•\` Pit: **${Pit}** \n\`•\` Housing: **${Housing}** \n\`•\` Murder Mystery: **${murderMystery}**` , value: `\u200B ` ,inline: true },
-    {name:`\`•\` Tnt Games: **${TNTGAMES}** \n\`•\` Blitz: **${Blitz}**\n\`•\` Warlords: **${Warlords}**\n\`•\` Classic Games: **${classicGames}**\n\`•\` Speed UHC: **${speedUHC}**\n\`•\` UHC: **${UHC}** \n\`•\` Prototype: **${prototype}** \n\`•\` Smash Heros: **${smashHeros}**\n\`•\` Mega Walls: **${megaWalls}**\n\`•\` Cops v Crims: **${MCGO}** ` , value: `\u200B` , inline: true},
-    )
+    for(game of gameCount){
+      embed.addField(gameCount[0], gameCount[1], true);
+    }
     message.reply({embed});
   })
 }
