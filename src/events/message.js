2e5f4a6cb5f7f41c749569f11daaf2dee04c926d
nathomp3 <nathomp3@ncsu.edu>
2021-12-30 11:15:10 -0500

    Changed filter to allow '.?,! special characters All others are filtered out. Words must now be alphabet only.

--- src/events/message.js
+++ src/events/message.js
@@ -2,8 +2,8 @@ const db = require('../index').db;
 module.exports = {
 	name: 'messageCreate',
 	async execute(message, client) {
-		words = message.content.toLowerCase().replace(/[^a-z0-9 ]/g, "").replace(/\s+/g, " ").split(" ").reduce(function(obj, name) {
-			if(!/^[a-z0-9]*$/.test(name))
+		words = message.content.toLowerCase().replace(/['.?,!]/g, "").replace(/\s+/g, " ").split(" ").reduce(function(obj, name) {
+			if(!/^[a-z]*$/.test(name))
 				return obj;
 			obj[name] = obj[name] ? ++obj[name] : 1;
 			return obj;
