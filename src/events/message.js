70ef881dec615f725bbb10b54cc616787798a12b
nathomp3 <nathomp3@ncsu.edu>
2021-12-29 20:59:47 -0500

    Updated regex (it works this time)

--- src/events/message.js
+++ src/events/message.js
@@ -2,7 +2,7 @@ const db = require('../index').db;
 module.exports = {
 	name: 'messageCreate',
 	async execute(message, client) {
-		words = message.content.toLowerCase().replace(/[^a-z0-9 ']/g, "").split(" ").reduce(function(obj, name) {
+		words = message.content.toLowerCase().replace(/[^a-z0-9 ]/g, "").replace(/\s+/g, " ").split(" ").reduce(function(obj, name) {
 			if(!/^[a-z0-9]*$/.test(name))
 				return obj;
 			obj[name] = obj[name] ? ++obj[name] : 1;
