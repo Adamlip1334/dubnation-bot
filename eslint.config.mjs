c8e07c1582be0e917c2969c6c4fa2bd19dbbe635
nathomp3 <nathomp3@ncsu.edu>
2025-10-03 19:53:10 -0400

    Run format

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,13 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.node },
+  },
+  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
 ]);
